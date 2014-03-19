bdf56ef500e2f7188ded4cc68275029181e45a9d
Devaev Maxim <mdevaev@gmail.com>
2014-03-19 17:25:20 +0400

    meters: configurable placeholders

--- meters/__init__.py
+++ meters/__init__.py
@@ -7,20 +7,18 @@ import time
 ##### Private objects #####
 _meters = {}
 _handlers = []
+_placeholders = {}
 #_watcher = _Watcher() # XXX: See bellow
 
 
 ##### Public methods #####
 def add_meter(name, meter):
-    assert callable(meter), "Meter must be a callable object"
     _meters[name] = meter
     return meter
 
 def get_meter(name):
     return _meters[name]
 
-
-###
 def add_handler(handler):
     assert hasattr(handler, "set_dumper")
     assert hasattr(handler, "start")
@@ -28,13 +26,23 @@ def add_handler(handler):
     assert hasattr(handler, "is_alive")
     handler.set_dumper(dump)
     _handlers.append(handler)
+    return handler
 
+def add_placeholder(name, placeholder):
+    _placeholders[name] = placeholder
+    return placeholder
+
+
+###
 def configure(config):
-    for handler_attrs in config.get("handlers", {}).values():
-        add_handler(_init_object(handler_attrs))
+    for (name, attrs) in config.get("placeholders", {}).items():
+        add_placeholder(name, _init_object(attrs, False))
 
-    for (meter_name, meter_attrs) in config.get("meters", {}).items():
-        add_meter(meter_name, _init_object(meter_attrs))
+    for attrs in config.get("handlers", {}).values():
+        add_handler(_init_object(attrs))
+
+    for (name, attrs) in config.get("meters", {}).items():
+        add_meter(name, _init_object(attrs))
 
 
 ###
@@ -51,14 +59,18 @@ def stop():
         _inner_stop()
 
 def dump():
+    # TODO: lazy placeholders
+    placeholders = { key: value() for (key, value) in _placeholders.items() }
+
     results = {}
     for (name, meter) in _meters.items():
         meter_value = meter()
         if isinstance(meter_value, dict): # For meters with multiple arrows (values)
             for (arrow, value) in meter_value.items():
-                results[_format_name(".".join((name, arrow)))] = value
+                meter_name = "{}.{}".format(name, arrow).format(**placeholders)
+                results[meter_name] = value
         else:
-            results[_format_name(name)] = meter_value
+            results[name.format(**placeholders)] = meter_value
     return results
 
 def is_running_hook():
@@ -69,31 +81,33 @@ def is_running_hook():
 
 
 ##### Private methods #####
-def _init_object(attrs):
-    assert isinstance(attrs, (str, dict)), "Only a string classname or dict"
-    if isinstance(attrs, str):
-        path = attrs
-        attrs = {}
-    else:
+def _init_object(attrs, enable_kwargs=True):
+    if isinstance(attrs, dict):
+        assert enable_kwargs, "Keyword arguments is disabled for {}".format(attrs)
         attrs = dict(attrs)
-        path = attrs.pop("class")
+        cls = attrs.pop("class")
+    else:
+        cls = attrs
+        attrs = {}
 
-    path = path.split(".")
-    assert len(path) >= 2, "Required package.class"
-    module_name = ".".join(path[:-1])
-    cls = getattr(importlib.import_module(module_name), path[-1])
+    if isinstance(cls, str):
+        path = cls.split(".")
+        assert len(path) >= 2, "Required package.class, not {}".format(cls)
+        module_name = ".".join(path[:-1])
+        cls = getattr(importlib.import_module(module_name), path[-1])
 
     if inspect.isclass(cls):
+        # Make objects from class
         obj = cls(**attrs)
-    else:
+    elif callable(cls):
         # Wrap non-class objects like a function to lambdas
         obj = ( lambda: cls(**attrs) )
+    else:
+        # Wrap variables to lambdas
+        assert len(attrs) == 0, "Variables does not accepts keyword arguments"
+        obj = ( lambda: cls )
 
     return obj
-
-def _format_name(name):
-    # TODO: Implement fqdn, node
-    return name
 
 
 ###
