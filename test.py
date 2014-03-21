543cb7379e5d7a34325382e9eb7e665446778a2f
Devaev Maxim <mdevaev@gmail.com>
2014-03-21 21:52:17 +0400

    test_scales -> test_scales_shared

--- test.py
+++ test.py
@@ -4,7 +4,7 @@
 import unittest
 
 # pylint: disable=W0401,W0614
-from meters.tests.test_scales import *
+from meters.tests.test_scales_shared import *
 from meters.tests.test_shortcuts import *
 
 
